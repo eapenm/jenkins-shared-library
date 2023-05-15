15ae05f5b5d71ce02c3fc1e2e13f3f86e675a2d4
Eapen Mani <eapenmani@gmail.com>
2023-05-15 18:04:43 +0530

    Added new function for docker build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,7 @@
 def call(String project,String tag, String hubUser){
     sh """
       docker image build -t ${hubUser}/${project} .
-      docker imgae tag ${hubUser}/${project} ${hubUser}/${project}:tag
-      docker imgae tag ${hubUser}/${project} ${hubUser}/${project}:latest
+      docker image tag ${hubUser}/${project} ${hubUser}/${project}:tag
+      docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
     """
 }
