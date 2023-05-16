7ce989c2d3705323667f7bf2509ac5204ab9356b
Eapen Mani <eapenmani@gmail.com>
2023-05-16 06:37:53 +0530

    Added script to push,scan the docker image in ecr

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,7 +1,14 @@
-def call(String project, String ImageTag, String hubUser){
+// def call(String project, String ImageTag, String hubUser){
     
+//     sh """   
+//      trivy image ${hubUser}/${project}:latest > scan.txt
+//      cat scan.txt
+//     """
+// }
+
+def call(String awsAccoutId,String region, String ecrRepoName){
     sh """   
-     trivy image ${hubUser}/${project}:latest > scan.txt
+     trivy image ${awsAccoutId}.dkr.ecr.${region}.amazonaws.com/${ecrRepoName}:latest > scan.txt
      cat scan.txt
     """
 }
