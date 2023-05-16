ab48bb9b5d537711d5a3ead449a69d4b48f04b5d
Eapen Mani <eapenmani@gmail.com>
2023-05-16 06:25:30 +0530

    Added function to push docker image to ECR

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,14 @@
-def call(String project,String tag, String hubUser){
+// def call(String project,String tag, String hubUser){
+//     sh """
+//       docker image build -t ${hubUser}/${project} .
+//       docker image tag ${hubUser}/${project} ${hubUser}/${project}:${tag}
+//       docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+//     """
+// }
+
+def call(String awsAccoutId,String region, String ecrRepoName){
     sh """
-      docker image build -t ${hubUser}/${project} .
-      docker image tag ${hubUser}/${project} ${hubUser}/${project}:${tag}
-      docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+      docker build -t ${ecrRepoName} .
+      docker tag ${ecrRepoName}:latest ${awsAccoutId}.dkr.${region}.amazonaws.com/${ecrRepoName}:latest
     """
 }
